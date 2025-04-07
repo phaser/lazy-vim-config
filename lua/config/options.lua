bcc7f638a1434f118422e51fd861d0b98891c531
phaser <cristian.bidea@gmail.com>
2025-04-07 17:30:26 +0300

    first commit

---
+++ lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local o = vim.o
+
+o.expandtab = true
+o.tabstop = 4
+o.softtabstop = -1
+o.shiftwidth = 0
+o.shiftround = true
+
+o.autoindent = true
+
